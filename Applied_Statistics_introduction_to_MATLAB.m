f0685547f1fd87d05d4a783594953968f0ee4b61
Nilufer Dastan <129163162+NiluferDastan@users.noreply.github.com>
2024-02-12 13:30:00 -0600

    This programme was uploaded

---
+++ Applied_Statistics_introduction_to_MATLAB.m
@@ -0,0 +1,19 @@
+%this is the command window
+%use it to execute commands line by line
+a = 243+543
+iskeyword matlab_var
+iskeyword image
+exist test_fun
+exist image
+clear a
+clear ans
+clear fid_flt fid_fxd
+clear all
+a=6
+b=9
+c=[1 2 3 4]
+who
+whos
+x=8.16
+A=[3 5 7; 1 2 3; 4 6 8];
+I=x*A;
